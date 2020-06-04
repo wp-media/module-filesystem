344df1711a835c7c565f6b33480910f45ff44ae3
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2020-06-04 09:30:44 +0200

    add structure to rocketCleanMinify.php fixture

--- Tests/Fixtures/inc/functions/rocketCleanMinify.php
+++ Tests/Fixtures/inc/functions/rocketCleanMinify.php
@@ -3,6 +3,64 @@
 return [
 	'vfs_dir'   => 'wp-content/cache/min/',
 
+	'structure' => [
+		'wp-content' => [
+
+			'cache' => [
+				// Minify cache.
+				'min'          => [
+					'1'         => [
+						'5c795b0e3a1884eec34a989485f863ff.js'     => '',
+						'5c795b0e3a1884eec34a989485f863ff.js.gz'  => '',
+						'fa2965d41f1515951de523cecb81f85e.css'    => '',
+						'fa2965d41f1515951de523cecb81f85e.css.gz' => '',
+						'wp-content'                              => [
+							'plugins' => [
+								'imagify' => [
+									'assets' => [
+										'css' => [
+											'admin-bar-924d9d45c4af91c09efb7ad055662025.css' => '',
+											'admin-bar-bce302f71910a4a126f7df01494bd6e0.css' => '',
+										],
+										'js'  => [
+											'admin-bar-171a2ef75c22c390780fe898f9d40c8d.js' => '',
+											'admin-bar-e4aa3c9df56ff024286f4df600f4c643.js' => '',
+										],
+									]
+								],
+							],
+						],
+						'wp-includes'                             => [
+							'css' => [
+								'admin-bar-85585a650224ba853d308137b9a13487.css' => '',
+								'dashicons-c2ba5f948753896932695bf9dad93d5e.css' => '',
+							],
+							'js'  => [
+								'jquery'                                        => [
+									'jquery-migrate-ca635e318ab90a01a61933468e5a72de.js' => '',
+								],
+								'admin-bar-65d8267e813dff6d0059914a4bc252aa.js' => '',
+							],
+						],
+					],
+					'2'         => [
+						'34a989485f863ff5c795b0e3a1884eec.js'     => '',
+						'34a989485f863ff5c795b0e3a1884eec.js.gz'  => '',
+						'523cecb81f85efa2965d41f1515951de.css'    => '',
+						'523cecb81f85efa2965d41f1515951de.css.gz' => '',
+					],
+					'3rd-party' => [
+						'2n7x3vd41f1515951de523cecb81f85e.css'    => '',
+						'2n7x3vd41f1515951de523cecb81f85e.css.gz' => '',
+						'bt937b0e3a1884eec34a989485f863ff.js'     => '',
+						'bt937b0e3a1884eec34a989485f863ff.js.gz'  => '',
+					],
+				],
+			],
+
+		]
+	],
+
 	// Test data.
 	'test_data' => [
 		'shouldNotCleanWhenNoExtensionsGiven'     => [
